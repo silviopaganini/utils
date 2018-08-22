a48e7549a2d422e16d9ce9b2275a99c617caad57
Silvio Paganini <silvio@fluuu.id>
2018-08-22 09:07:51 +0100

    es6 shouldnt be babel es2015

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,23 @@
 import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
-// import minify from 'rollup-plugin-babel-minify'
 
 export default [
   {
     input: 'src/index.js',
     plugins: [
       babel({ presets: ['es2015-rollup'], babelrc: false })
-      // minify({
-      //   comments: false
-      // })
     ],
     output: [
       {
         name: 'futils',
         file: pkg.browser,
         format: 'umd'
-      },
+      }
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
     ]
